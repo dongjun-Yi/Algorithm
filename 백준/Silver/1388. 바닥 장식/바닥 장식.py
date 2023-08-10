a6742383d0be680affbe201a574ba195c1c9637c
dongjun-Yi <90665186+dongjun-Yi@users.noreply.github.com>
2023-08-10 13:38:13 +0900

    [Silver IV] Title: 바닥 장식, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1388. 바닥 장식/바닥 장식.py
@@ -0,0 +1,40 @@
+import sys
+
+sys.setrecursionlimit(1000000)
+n, m = map(int, input().split())
+
+graph = []
+visited = [[False] * m for _ in range(n)]
+
+for _ in range(n):
+  graph.append(list(input()))
+
+# 좌우
+dy = [-1, 1]
+# 상하
+dx = [-1, 1]
+cnt = 0
+
+
+def dfs(x, y):
+  visited[x][y] = True
+
+  if graph[x][y] == '|':
+    if x + 1 < n and graph[x + 1][y] == '|' and not visited[x + 1][y]:
+      dfs(x + 1, y)
+    else:
+      return
+  if graph[x][y] == '-':
+    if y + 1 < m and graph[x][y + 1] == '-' and not visited[x][y + 1]:
+      dfs(x, y + 1)
+    else:
+      return
+
+
+for i in range(n):
+  for j in range(m):
+    if not visited[i][j]:
+      dfs(i, j)
+      cnt += 1
+
+print(cnt)
