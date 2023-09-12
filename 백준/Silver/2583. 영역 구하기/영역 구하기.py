717ee4fa3454945399a9957a37f34165db28fd2e
dongjun-Yi <90665186+dongjun-Yi@users.noreply.github.com>
2023-09-12 16:18:52 +0900

    Update 영역 구하기.py

--- 백준/Silver/2583. 영역 구하기/영역 구하기.py
+++ 백준/Silver/2583. 영역 구하기/영역 구하기.py
@@ -1,6 +1,7 @@
 from collections import deque
-
+# 세로, 가로, 입력받을 영역의 개수
 m, n, k = map(int, input().split())
+
 graph = [[0] * n for _ in range(m)]
 
 for _ in range(k):
@@ -8,9 +9,10 @@ for _ in range(k):
   for i in range(left_y, right_y):
     for j in range(left_x, right_x):
       graph[i][j] += 1
+
 # 상하좌우
 dx = [0, 0, -1, 1]
-dy = [1, -1, 0, 0]
+dy = [-1, 1, 0, 0]
 
 
 def bfs(x, y):
@@ -18,6 +20,7 @@ def bfs(x, y):
   cnt = 1
 
   while q:
+    # 가로, 세로
     y, x = q.popleft()
 
     for i in range(4):
@@ -26,7 +29,6 @@ def bfs(x, y):
 
       if nx < 0 or nx >= n or ny < 0 or ny >= m:
         continue
-
       if graph[ny][nx] == 0:
         graph[ny][nx] = 1
         q.append((ny, nx))
@@ -34,15 +36,16 @@ def bfs(x, y):
   return cnt
 
 
-res = []
+result = []
+
 for i in range(m):
   for j in range(n):
     if graph[i][j] == 0:
       graph[i][j] = 1
-      res.append(bfs(i, j))
+      result.append(bfs(i, j))
 
-print(len(res))
-res.sort()
+print(len(result))
 
-for x in res:
-  print(x, end=' ')
+result.sort()
+
+print(*result)
