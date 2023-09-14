42273138883df7ac2321932c938086ae92eaa3ab
dongjun-Yi <90665186+dongjun-Yi@users.noreply.github.com>
2023-09-14 15:13:59 +0900

    [Gold V] Title: 토마토, Time: 1276 ms, Memory: 98640 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.py
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -6,41 +6,43 @@ graph = []
 for _ in range(n):
   graph.append(list(map(int, input().split())))
 
-q = deque()
-for i in range(n):
-  for j in range(m):
-    if graph[i][j] == 1:
-      q.append((i, j))
-
 # 상하좌우
-dx = [-1, 1, 0, 0]
+dx = [1, -1, 0, 0]
 dy = [0, 0, -1, 1]
 
 
 def bfs():
+
   while q:
-    cur = q.popleft()
+    x, y = q.popleft()
 
     for i in range(4):
-      nx = cur[0] + dx[i]
-      ny = cur[1] + dy[i]
+      nx = x + dx[i]
+      ny = y + dy[i]
 
       if nx < 0 or nx >= n or ny < 0 or ny >= m:
         continue
 
       if graph[nx][ny] == 0:
-        graph[nx][ny] = graph[cur[0]][cur[1]] + 1
+        graph[nx][ny] = graph[x][y] + 1
         q.append((nx, ny))
 
 
+q = deque()
+
+for i in range(n):
+  for j in range(m):
+    if graph[i][j] == 1:
+      q.append((i, j))
+
 bfs()
-res = 0
+ans = 0
 
-for i in graph:
-  for j in i:
-    if j == 0:
+for row in graph:
+  for tomato in row:
+    if tomato == 0:
       print(-1)
-      exit(0)
-  res = max(res, max(i))
+      exit()
+  ans = max(ans, max(row))
 
-print(res - 1)
+print(ans - 1)
