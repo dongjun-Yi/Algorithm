c7561f488c7e93df056813dd3620125e5775f800
dongjun-Yi <90665186+dongjun-Yi@users.noreply.github.com>
2023-11-02 15:42:13 +0900

    [level 1] Title: 옹알이 (2), Time: 0.06 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.py
@@ -0,0 +1,12 @@
+def solution(babbling):
+    answer = 0
+    words = ["aya", "ye", "woo", "ma"]
+    for b in babbling:
+        for w in words:
+            if w * 2 not in b:
+                b = b.replace(w, ' ')
+                
+        if b.isspace():
+            answer+=1
+        
+    return answer
