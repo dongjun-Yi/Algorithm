4130efba6fc167b58d1297f7d86bb5ad65514c4b
dongjun-Yi <90665186+dongjun-Yi@users.noreply.github.com>
2023-11-17 15:38:52 +0900

    [level 2] Title: [1차] 캐시, Time: 0.98 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.py
@@ -0,0 +1,29 @@
+from collections import deque
+import sys
+def solution(cacheSize, cities):
+    answer = 0 
+    d = []
+    
+    if cacheSize == 0:
+        answer = 5 * len(cities)
+        return answer
+    
+    for i in range(len(cities)):
+        # 대소문자 구분 안하므로
+        city = cities[i].lower()
+        
+        # cahce miss
+        if city not in d:
+            if len(d) == cacheSize:
+                # LRU 알고리즘을 사용하므로 cache에서 가장 오래된거 삭제
+                d.pop(0)
+            d.append(city)
+            answer+=5
+            
+        # cache hit
+        else:
+            index = d.index(city)
+            d.append(d.pop(index))
+            answer+=1
+
+    return answer
