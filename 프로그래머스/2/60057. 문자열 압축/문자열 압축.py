e1c1b7bc337eea8509ade27492c470cad7180ed9
dongjun-Yi <90665186+dongjun-Yi@users.noreply.github.com>
2023-11-23 14:47:03 +0900

    문자열 압축 풀이 수정.py

--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.py
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.py
@@ -8,16 +8,22 @@ def solution(s):
         tmp = s[:i]
         cnt = 1
         # 가장 짧은 문자열 찾기
-        for j in range(i,len(s)+i, i):
+        for j in range(i,len(s), i):
             if tmp == s[j:j+i]:
                 cnt +=1
             else:
-                if cnt != 1:
+                if cnt >=2:
                     b += str(cnt) + tmp
                 else:
                     b+= tmp
                 # 기준 문자 바꾸기
                 tmp = s[j:j+i]
                 cnt = 1
+        # 남아있는 문자열에 대해 처리
+        if cnt >= 2:
+            b += str(cnt) + tmp
+        else:
+            b += tmp
+        
         answer = min(answer, len(b))
     return answer
