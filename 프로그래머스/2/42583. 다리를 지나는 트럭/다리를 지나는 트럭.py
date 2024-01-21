0d9bb75617c82b15a74d45189eded675089690e2
dongjun-Yi <90665186+dongjun-Yi@users.noreply.github.com>
2024-01-21 20:30:19 +0900

    다리를 지나는 트럭 문제 문제풀이 방식 주석 코멘트 작성.

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
@@ -1,3 +1,7 @@
+# truck_weights <= 10,000, bridge_length <=10,000이므로 시간 복잡도 O(n)이하로 설계해야함.
+# sum()은 시간복잡도가 O(n)이므로 sum(bridge)로 구현했을 경우 O(n^2)이 되어서 O(10,000 * 10,000)이 되므로 시간초과 발생.
+# 따라서 curren_weight 변수로 현재 다리에 있는 무게를 측정하고, 다리를 건넌 차를 가감하는 형식으로 구현.
+
 from collections import deque
 
 def solution(bridge_length, weight, truck_weights):
