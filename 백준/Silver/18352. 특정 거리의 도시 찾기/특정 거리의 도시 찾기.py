980667b3c9c898cd0e93bb236df317e293ce6db2
dongjun-Yi <90665186+dongjun-Yi@users.noreply.github.com>
2024-03-03 21:04:55 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 1820 ms, Memory: 99940 KB -BaekjoonHub

--- 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
@@ -1,35 +1,31 @@
 import sys
 from collections import deque
-n,m,k,x = map(int, sys.stdin.readline().split())
 
-graph = [[] for _ in range(n+1)]
-visited = [0] * (n+1)
-def bfs(x):
-  q = deque([x])
-  cnt = 0
-  visited[x] = 1
-  while q:
-    cur = q.popleft()
-    for node in graph[cur]:
-      if visited[node] == 0:
-        visited[node] = visited[cur] + 1
-        q.append(node)
-    
+n, m, k, x = map(int, sys.stdin.readline().split())
+
+g = [[] for _ in range(n + 1)]
+
 for _ in range(m):
-  a,b = map(int, sys.stdin.readline().split())
-  graph[a].append(b)
+  a, b = map(int, sys.stdin.readline().split())
+  g[a].append(b)
 
-bfs(x)
-for i in range(len(visited)):
-  visited[i]-=1
-res = []
+distance = [-1] * (n + 1)
+distance[x] = 0
 
-for i in range(1, len(visited)):
-  if visited[i] == k:
-    res.append(i)
+q = deque([x])
 
-if res:
-  for x in res:
-    print(x)
-else:
+while q:
+  now = q.popleft()
+  for next_node in g[now]:
+    if distance[next_node] == -1:  # 아직 방문하지 않은 노드라면
+      distance[next_node] = distance[now] + 1
+      q.append(next_node)
+
+check = False
+for i in range(1, n + 1):
+  if distance[i] == k:
+    print(i)
+    check = True
+
+if check == False:
   print(-1)
