d2e62826bea44bdeb83f7b36b70141c9151ac5d7
dongjun-Yi <90665186+dongjun-Yi@users.noreply.github.com>
2024-03-14 20:48:42 +0900

    [level 4] Title: 무지의 먹방 라이브, Time: 141.63 ms, Memory: 43.5 MB -BaekjoonHub

--- 프로그래머스/4/42891. 무지의 먹방 라이브/무지의 먹방 라이브.py
+++ 프로그래머스/4/42891. 무지의 먹방 라이브/무지의 먹방 라이브.py
@@ -4,19 +4,19 @@ def solution(food_times, k):
     if sum(food_times) <= k:
         return -1
     
-    q = []
-    for i in range(len(food_times)):
-        heapq.heappush(q, (food_times[i], i+1))
-
-    time_sum = 0
-    table_round_count = 0
     length = len(food_times)
-    
-    while time_sum + ((q[0][0] - table_round_count) * length) <= k:
-        now = heapq.heappop(q)[0] 
-        time_sum += (now - table_round_count) * length
-        length -=1
-        table_round_count = now
+    q = []
+    for i in range(length):
+        heapq.heappush(q, (food_times[i], i + 1))
         
-    answer = sorted(q, key = lambda x : x[1])
-    return answer[(k-time_sum) % length][1]
+    round = 0
+    s = 0
+    
+    while s + ((q[0][0] - round) * length) <= k:
+        now = heapq.heappop(q)[0]
+        s += (now - round) * length
+        length -=1
+        round = now
+
+    answer = sorted(q, key = lambda x: x[1])
+    return answer[(k-s)%length][1]
