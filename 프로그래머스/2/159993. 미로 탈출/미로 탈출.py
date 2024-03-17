8d51c62be9cd9070d3aefc3b1b76dd9f5ee04144
dongjun-Yi <90665186+dongjun-Yi@users.noreply.github.com>
2024-03-17 14:29:00 +0900

    [level 2] Title: 미로 탈출, Time: 0.07 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/159993. 미로 탈출/미로 탈출.py
+++ 프로그래머스/2/159993. 미로 탈출/미로 탈출.py
@@ -1,16 +1,15 @@
 from collections import deque
-
 def bfs(start, end, maps):
-    n, m = len(maps), len(maps[0])
+    n = len(maps)
+    m = len(maps[0])
     visited = [[False] * m for _ in range(n)]
-    
+
     for i in range(n):
         for j in range(m):
             if maps[i][j] == start:
-                q = deque([(i,j, 0)])
+                q = deque([(i,j,0)])
                 visited[i][j] = True
-                break
-    # 동남서북            
+    # 동남서북
     dx = [0,1,0,-1]
     dy = [1,0,-1,0]
     
@@ -24,21 +23,18 @@ def bfs(start, end, maps):
             nx = x + dx[i]
             ny = y + dy[i]
             
-            if nx < 0 or nx >=n or ny < 0 or ny>=m:
+            if nx < 0 or nx >=n or ny <0 or ny>=m:
                 continue
-                
             if maps[nx][ny] != 'X' and not visited[nx][ny]:
                 visited[nx][ny] = True
-                q.append((nx,ny, time + 1))
-    
+                q.append((nx, ny, time + 1))
     return -1
 
 def solution(maps):
+    path1 = bfs('S','L', maps)
+    path2 = bfs('L','E', maps)
 
-    path1 = bfs('S', 'L', maps)
-    path2 = bfs('L', 'E', maps)
-    
     if path1 != -1 and path2 != -1:
         return path1 + path2
     
-    return -1 
+    return -1
