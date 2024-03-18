735e92ec46c3d275c57f2c1a4a3ec71ac32d1c92
dongjun-Yi <90665186+dongjun-Yi@users.noreply.github.com>
2024-03-18 15:51:31 +0900

    [level 2] Title: 리코쳇 로봇, Time: 1.38 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.py
@@ -0,0 +1,41 @@
+from collections import deque
+import sys
+
+def solution(board):
+    answer = 0
+    n = len(board)
+    m = len(board[0])
+    distance = [[sys.maxsize] * m for _ in range(n)]
+
+    for i in range(n):
+        for j in range(m):
+            if board[i][j] == 'R':
+                q = deque([(i, j, 0)])
+                distance[i][j] = 0
+    
+    # 동서남북
+    dx = [0,0,-1,1]
+    dy = [1,-1,0,0]
+    direction = 0
+    turn_cnt = 0
+    time = 0
+    
+    while q:
+        x, y, time = q.popleft()
+        
+        if board[x][y] == 'G':
+            return time
+        
+        for i in range(4):
+            nx = x
+            ny = y
+            
+            while 0<=nx + dx[i]<n and 0<=ny + dy[i]<m and board[nx + dx[i]][ny+dy[i]] != 'D':
+                nx += dx[i]
+                ny += dy[i]
+                
+            if distance[nx][ny] > time + 1:
+                distance[nx][ny] = time + 1
+                q.append((nx,ny, time + 1))
+        
+    return -1
