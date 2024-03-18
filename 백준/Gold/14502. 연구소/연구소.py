e2caefe92fd35561658b83713edf42489cec0d7a
dongjun-Yi <90665186+dongjun-Yi@users.noreply.github.com>
2024-03-18 20:51:37 +0900

    [Gold IV] Title: 연구소, Time: 2848 ms, Memory: 33756 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.py
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -1,62 +1,56 @@
 from itertools import combinations
-import sys, copy
+import copy, sys
 
 n, m = map(int, input().split())
-
-g = [list(map(int, input().split())) for _ in range(n)]
-
-blank = []
-
+walls = []
+g = []
+virus = []
 for i in range(n):
+  g.append(list(map(int, input().split())))
   for j in range(m):
     if g[i][j] == 0:
-      blank.append((i, j))
+      walls.append((i, j))
+    if g[i][j] == 2:
+      virus.append((i, j))
 
-# 상하좌우
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
+# 동서남북
+dx = [0, 0, 1, -1]
+dy = [1, -1, 0, 0]
 
 
 def dfs(x, y):
-  visited[x][y] = True
-
   for i in range(4):
     nx = x + dx[i]
     ny = y + dy[i]
 
-    if nx < 0 or nx >= n or ny < 0 or ny >= m:
+    if nx < 0 or nx >= n or ny < 0 or ny >= m or new_g[nx][ny] == 1:
       continue
 
-    if new_g[nx][ny] == 1:
-      continue
-
-    if new_g[nx][ny] == 0 and not visited[nx][ny]:
+    if new_g[nx][ny] == 0:
       new_g[nx][ny] = 2
       dfs(nx, ny)
 
 
-res = -sys.maxsize
-
-for c in list(combinations(blank, 3)):
-  cnt = 0
-  t = []
-  visited = [[False] * m for _ in range(n)]
+result = -sys.maxsize
+for wall in list(combinations(walls, 3)):
   new_g = copy.deepcopy(g)
-
+  cnt = 0
+  visited = [[False] * m for _ in range(n)]
+  
   for i in range(3):
-    cx, cy = c[i]
-    new_g[cx][cy] = 1
+    x, y = wall[i]
+    new_g[x][y] = 1
 
-  for i in range(n):
-    for j in range(m):
-      if new_g[i][j] == 2:
-        t.append((i, j))
-        dfs(i, j)
+  for i in range(len(virus)):
+    x, y = virus[i]
+    visited[x][y] = True
+    dfs(x, y)
 
   for i in range(n):
     for j in range(m):
       if new_g[i][j] == 0:
         cnt += 1
 
-  res = max(res, cnt)
-print(res)
+  result = max(result, cnt)
+
+print(result)
