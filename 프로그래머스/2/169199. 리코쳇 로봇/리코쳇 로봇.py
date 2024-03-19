0c123f536be8e49ffc91d27992e6f492e24a5526
dongjun-Yi <90665186+dongjun-Yi@users.noreply.github.com>
2024-03-19 17:05:47 +0900

    [level 2] Title: 리코쳇 로봇, Time: 1.32 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.py
+++ 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.py
@@ -5,20 +5,19 @@ def solution(board):
     answer = 0
     n = len(board)
     m = len(board[0])
+    q = deque()
+    
     distance = [[sys.maxsize] * m for _ in range(n)]
-
+    
     for i in range(n):
         for j in range(m):
             if board[i][j] == 'R':
-                q = deque([(i, j, 0)])
+                q.append((i,j,0))
                 distance[i][j] = 0
     
-    # 동서남북
-    dx = [0,0,-1,1]
-    dy = [1,-1,0,0]
-    direction = 0
-    turn_cnt = 0
-    time = 0
+    # 상하좌우
+    dx = [-1,1,0,0]
+    dy = [0,0,-1,1]
     
     while q:
         x, y, time = q.popleft()
@@ -28,14 +27,14 @@ def solution(board):
         
         for i in range(4):
             nx = x
-            ny = y
+            ny = y 
             
-            while 0<=nx + dx[i]<n and 0<=ny + dy[i]<m and board[nx + dx[i]][ny+dy[i]] != 'D':
+            while 0<=nx + dx[i]<n and 0<=ny + dy[i]<m and board[nx+dx[i]][ny+dy[i]] != 'D':
                 nx += dx[i]
                 ny += dy[i]
-                
+            
             if distance[nx][ny] > time + 1:
                 distance[nx][ny] = time + 1
-                q.append((nx,ny, time + 1))
-        
+                q.append((nx, ny, time + 1))
+                
     return -1
