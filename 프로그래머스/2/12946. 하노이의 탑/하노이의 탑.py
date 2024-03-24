003ec67d87b4f27a5cc70e333492856b2e3cfd87
dongjun-Yi <90665186+dongjun-Yi@users.noreply.github.com>
2024-03-24 21:46:40 +0900

    [level 2] Title: 하노이의 탑, Time: 11.29 ms, Memory: 18.8 MB -BaekjoonHub

--- 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.py
+++ 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.py
@@ -1,11 +1,14 @@
 def solution(n):
     answer = []
-    def hanoi(src, dest, mid, n):
-        if n == 1:
-            answer.append([src, dest])
-        else:
-            hanoi(src, mid, dest, n-1)
-            hanoi(src, dest, mid, 1)
-            hanoi(mid, dest, src, n-1)
+    
+    def hanoi(start, end, mid, n):
+        if n == 1: # base case
+            answer.append([start, end])
+        else: # general case
+            hanoi(start, mid, end, n-1)
+            hanoi(start, end, mid, 1)
+            hanoi(mid, end, start, n-1)
+            
     hanoi(1, 3, 2, n)
+            
     return answer
