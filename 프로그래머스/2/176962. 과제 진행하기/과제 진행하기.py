d24dd31cab61f4748d535bfd0d23effd94b7cd84
dongjun-Yi <90665186+dongjun-Yi@users.noreply.github.com>
2024-03-31 14:36:28 +0900

    [level 2] Title: 과제 진행하기, Time: 1.61 ms, Memory: 11 MB -BaekjoonHub

--- 프로그래머스/2/176962. 과제 진행하기/과제 진행하기.py
+++ 프로그래머스/2/176962. 과제 진행하기/과제 진행하기.py
@@ -1,41 +1,38 @@
-from collections import deque
-
 def convert_time(s):
-    h, m = map(int, s.split(':'))
-    return h * 60 + m
+    hour, time = map(int, s.split(":"))
+    return hour * 60 + time
 
 def solution(plans):
     answer = []
     new_plans = []
+    for name, start, playtime in plans:    
+        new_plans.append((name, convert_time(start), int(playtime)))
+
+    new_plans.sort(key = lambda x: x[1])
+    time = 0
+    homework = [] # 선입후출 : 스택 자료구조 이용
     
-    for name, start, playtime in plans:
-        new_plans.append([name, convert_time(start), int(playtime)])
-    
-    new_plans.sort(key = lambda x : x[1])
-    homework = deque()
-    left_time = 0
-        
     for i in range(len(new_plans)):
         name, start, playtime = new_plans[i]
-        
+
         while homework:
             h_name, h_playtime = homework.pop()
 
-            if left_time >= h_playtime:
-                left_time -= h_playtime
+            if time >= h_playtime:
                 answer.append(h_name)
+                time -= h_playtime
             else:
-                homework.append((h_name, h_playtime - left_time))
+                homework.append((h_name, h_playtime - time))
                 break
-                
-        homework.append((name, playtime))
-        
-        if i < len(plans) - 1: # 마지막 요소가 아니라면
-            next_start = new_plans[i+1][1]
-            left_time = next_start - start
 
+        homework.append((name, playtime))
+
+        if i < len(new_plans) - 1:
+            next = new_plans[i+1][1]
+            time = next - start
+                
     while homework:
         name = homework.pop()[0]
         answer.append(name)
-        
+    
     return answer
