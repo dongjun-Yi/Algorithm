3cb819f383542ce78584e5ff263e5a4145f197ff
dongjun-Yi <90665186+dongjun-Yi@users.noreply.github.com>
2024-04-16 16:09:57 +0900

    [Silver V] Title: 수들의 합 5, Time: 296 ms, Memory: 57064 KB -BaekjoonHub

--- 백준/Silver/2018. 수들의 합 5/수들의 합 5.java
+++ 백준/Silver/2018. 수들의 합 5/수들의 합 5.java
@@ -1,39 +1,35 @@
 import java.util.*;
-import java.io.*;
 
 public class Main {
-  public static void main(String[] args) throws IOException {
+
+  public static void main(String[] args){
     Scanner sc = new Scanner(System.in);
-
     int n = sc.nextInt();
-    sc.close();
-    
-    int[] arr = new int[n + 1];
 
-    for (int i = 0; i <= n; i++) {
-      arr[i] = i;
+    int[] a = new int[n+1];
+
+    for(int i = 0; i < n; i++){
+      a[i] = i+1;
     }
+    int lt = 0;
+    int rt = 0;
+    int sum = a[0];
+    int result = 0;
 
-    int lt = 1;
-    int rt = 1;
-    int sum_value = 1;
-    int cnt = 0;
-
-    while (rt < n) {
-      if (sum_value < n) {
+    while(rt < n){
+      if(sum < n){
         rt++;
-        sum_value += arr[rt];
-      } else if (sum_value > n) {
-        sum_value -= arr[lt];
+        sum += a[rt];
+      } else if(sum > n){
+        sum -= a[lt];
         lt++;
-      } else {
-        cnt++;
+      } else{
+        result++;
         rt++;
-        sum_value += arr[rt];
+        sum += a[rt];
       }
     }
 
-    System.out.println(cnt + 1);
-
+    System.out.println(result);
   }
 }
