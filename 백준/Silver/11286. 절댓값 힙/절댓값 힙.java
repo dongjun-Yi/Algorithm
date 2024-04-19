1c0ba1be531bce11bf0bb806d0e59c6e2c2a05e2
dongjun-Yi <90665186+dongjun-Yi@users.noreply.github.com>
2024-04-19 16:15:27 +0900

    [Silver I] Title: 절댓값 힙, Time: 1356 ms, Memory: 103788 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -1,33 +1,34 @@
 import java.util.*;
 
 public class Main {
+
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     int n = sc.nextInt();
 
-    PriorityQueue<Integer> q = new PriorityQueue<Integer>(new MyComparator());
+    PriorityQueue<Integer> pq = new PriorityQueue<Integer>(new Comparator<Integer>() {
+      public int compare(Integer i1, Integer i2) {
+        if (Math.abs(i1) == Math.abs(i2)) {
+          return i1 - i2;
+        }
+        return Math.abs(i1) - Math.abs(i2);
+      }
+    });
 
     for (int i = 0; i < n; i++) {
-      int cur = sc.nextInt();
+      int x = sc.nextInt();
 
-      if (cur == 0) {
-        if (q.isEmpty()) {
+      if (x != 0) {
+        pq.offer(x);
+      } else {
+        if (pq.isEmpty()){
           System.out.println(0);
           continue;
         }
-        System.out.println(q.poll());
-      } else {
-        q.add(cur);
+          
+        System.out.println(pq.poll());
       }
     }
-  }
-}
 
-class MyComparator implements Comparator<Integer> {
-  public int compare(Integer i1, Integer i2) {
-    if (Math.abs(i1) == Math.abs(i2)) {
-      return i1 > i2 ? 1 : -1;
-    }
-    return Math.abs(i1) - Math.abs(i2);
   }
 }
