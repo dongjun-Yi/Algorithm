8fe8bd1b813214e6b4744c6800c66a7ea0ea7de8
dongjun-Yi <90665186+dongjun-Yi@users.noreply.github.com>
2024-04-30 21:17:46 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 204 ms, Memory: 17660 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
@@ -1,27 +1,28 @@
 import java.util.*;
 
 public class Main {
-
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    String[] s = sc.nextLine().split("-");
-
+    String[] exp = sc.nextLine().split("-");
     int result = 0;
-    for (int i = 0; i < s.length; i++) {
-      int temp = sum_pa(s[i]);
+    
+    for (int i = 0; i < exp.length; i++) {
+      int num = makeNum(exp[i]);
 
-      if (i == 0) {
-        result = temp;
+      if (i == 0){ // 첫 번째 숫자는 result 변수에 담음
+        result = num;
         continue;
       }
-      result -= temp;
+      result -= num; // 최소값 갱신
     }
+
     System.out.println(result);
   }
 
-  public static int sum_pa(String s) {
+  static int makeNum(String s) {
     String[] nums = s.split("\\+");
     int sum = 0;
+
     for (int i = 0; i < nums.length; i++) {
       sum += Integer.parseInt(nums[i]);
     }
