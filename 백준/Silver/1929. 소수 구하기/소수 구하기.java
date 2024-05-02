1658eabf259d75bbe83588a1182f71b346007aa1
dongjun-Yi <90665186+dongjun-Yi@users.noreply.github.com>
2024-05-02 14:37:27 +0900

    [Silver III] Title: 소수 구하기, Time: 380 ms, Memory: 29864 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -1,34 +1,27 @@
 import java.util.*;
 
 public class Main {
-
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    StringTokenizer st = new StringTokenizer(sc.nextLine());
+    int m = sc.nextInt();
+    int n = sc.nextInt();
 
-    int m = Integer.parseInt(st.nextToken());
-    int n = Integer.parseInt(st.nextToken());
+    int[] A = new int[n + 1];
 
-    int[] arr = new int[n + 1];
-
-    for (int i = 2; i <= n; i++) {
-      arr[i] = i;
-    }
+    for (int i = 2; i <= n; i++)
+      A[i] = i;
 
     for (int i = 2; i <= Math.sqrt(n); i++) {
-      if (arr[i] == 0)
+      if (A[i] == 0)
         continue;
-
-      for (int j = i + i; j <= n; j = j + i) {
-        arr[j] = 0;
-      }
+      for (int j = i + i; j <= n; j = j + i)
+        A[j] = 0;
     }
 
     StringBuffer sb = new StringBuffer();
     for (int i = m; i <= n; i++) {
-      if (arr[i] != 0) {
-        sb.append(i + "\n");
-      }
+      if (A[i] != 0)
+        sb.append(A[i] + "\n");
     }
     System.out.println(sb);
   }
