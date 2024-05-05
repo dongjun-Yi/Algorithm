561f4e5fad273859839f4005a451f897d2efd456
dongjun-Yi <90665186+dongjun-Yi@users.noreply.github.com>
2024-05-05 16:35:25 +0900

    [Gold IV] Title: 이분 그래프, Time: 984 ms, Memory: 269452 KB -BaekjoonHub

--- 백준/Gold/1707. 이분 그래프/이분 그래프.java
+++ 백준/Gold/1707. 이분 그래프/이분 그래프.java
@@ -4,65 +4,56 @@ import java.io.*;
 public class Main {
   static ArrayList<Integer>[] g;
   static boolean[] visited;
-  static boolean[] check;
+  static int[] group;
+  static boolean even;
 
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
     int t = Integer.parseInt(br.readLine());
-    StringTokenizer st;
 
     for (int i = 0; i < t; i++) {
-      st = new StringTokenizer(br.readLine());
-
+      StringTokenizer st = new StringTokenizer(br.readLine());
       int v = Integer.parseInt(st.nextToken());
       int e = Integer.parseInt(st.nextToken());
-
       g = new ArrayList[v + 1];
-      check = new boolean[v + 1];
-
-      for (int j = 1; j < v + 1; j++) {
-        g[j] = new ArrayList<Integer>();
-      }
       visited = new boolean[v + 1];
+      group = new int[v + 1];
+      even = true;
 
-      for (int k = 0; k < e; k++) {
+      for (int j = 0; j <= v; j++) {
+        g[j] = new ArrayList();
+      }
+
+      for (int j = 0; j < e; j++) {
         st = new StringTokenizer(br.readLine());
         int a = Integer.parseInt(st.nextToken());
         int b = Integer.parseInt(st.nextToken());
-
         g[a].add(b);
         g[b].add(a);
       }
 
-      boolean even = true;
-
-      for (int j = 1; j < v + 1; j++) {
-        if (!dfs(j)) {
-          even = false;
+      for (int j = 1; j <= v; j++) {
+        if (even == false) {
           break;
         }
+        dfs(j);
       }
-
-      if (even == false) {
+      if (even == false)
         System.out.println("NO");
-      } else {
+      else
         System.out.println("YES");
-      }
     }
   }
 
-  public static boolean dfs(int v) {
+  static void dfs(int v) {
     visited[v] = true;
+
     for (int cur : g[v]) {
       if (!visited[cur]) {
-        check[cur] = !check[v];
+        group[cur] = (group[v] + 1) % 2;
         dfs(cur);
-      } else {
-        if (check[cur] == check[v])
-          return false;
-      }
+      } else if (group[cur] == group[v])
+        even = false;
     }
-    return true;
   }
 }
