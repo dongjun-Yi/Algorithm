c1500c8ce605c53cc6f1660a752284ea5fe3c4ab
dongjun-Yi <90665186+dongjun-Yi@users.noreply.github.com>
2024-05-15 20:30:58 +0900

    [Silver IV] Title: 문자열 집합, Time: 1152 ms, Memory: 69132 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.java
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.java
@@ -1,62 +1,27 @@
 import java.util.*;
 
 public class Main {
-  static Scanner sc;
-
   public static void main(String[] args) {
-    sc = new Scanner(System.in);
+    Scanner sc = new Scanner(System.in);
     int n = sc.nextInt();
     int m = sc.nextInt();
 
-    Trie root = new Trie();
-    makeTrie(root, n);
-    int result = findStrings(root, m);
+    Set<String> set = new TreeSet();
+
+    for (int i = 0; i < n; i++) {
+      set.add(sc.next());
+    }
+
+    int result = 0;
+
+    for (int i = 0; i < m; i++) {
+      String s = sc.next();
+
+      if (set.contains(s)) {
+        result++;
+      }
+    }
+
     System.out.println(result);
   }
-
-  static int findStrings(Trie root, int m) {
-    int count = 0;
-    while (m > 0) {
-      String text = sc.next();
-      Trie now = root;
-
-      for (int i = 0; i < text.length(); i++) {
-        char c = text.charAt(i);
-        int index = c - 'a';
-        if (now.next[index] == null)
-          break;
-
-        now = now.next[index];
-        if (i == text.length() - 1 && now.isEnd == true) {
-          count++;
-        }
-      }
-      m--;
-    }
-    return count;
-  }
-
-  static void makeTrie(Trie root, int n) {
-    while (n > 0) {
-      String text = sc.next();
-      Trie now = root;
-
-      for (int i = 0; i < text.length(); i++) {
-        char c = text.charAt(i);
-        int index = c - 'a';
-        if (now.next[index] == null) {
-          now.next[index] = new Trie();
-        }
-        now = now.next[index];
-        if (i == text.length() - 1)
-          now.isEnd = true;
-      }
-      n--;
-    }
-  }
-}
-
-class Trie {
-  Trie[] next = new Trie[26];
-  boolean isEnd;
 }
