e2f9d03b29120ae57350978ba6f77bdfd2e2d501
dongjun-Yi <90665186+dongjun-Yi@users.noreply.github.com>
2024-05-16 15:45:00 +0900

    [Silver III] Title: 퇴사, Time: 212 ms, Memory: 17712 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.java
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -1,33 +1,26 @@
 import java.util.*;
-public class Main {
 
+public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     int n = sc.nextInt();
+    int[] T = new int[n + 1];
+    int[] P = new int[n + 1];
+    int[] D = new int[n + 2];
 
-    int[] T = new int[n+1];
-    int[] P = new int[n+1];
-
-    sc.nextLine();
-
-    StringTokenizer st;
-    for (int i = 1; i<= n;i ++){
-      st = new StringTokenizer(sc.nextLine());
-      int a = Integer.parseInt(st.nextToken());
-      int b = Integer.parseInt(st.nextToken());
-      T[i] = a;
-      P[i] = b;
+    for (int i = 1; i <= n; i++) {
+      T[i] = sc.nextInt();
+      P[i] = sc.nextInt();
     }
-    
-    int[] D = new int[n+2];
 
-    for (int i = n; i > 0; i--){
-      if (i + T[i] <= n + 1){
-        D[i] = Math.max(D[i+1], D[i+T[i]] + P[i]);
-      } else{
-        D[i] = D[i+1];
+    for (int i = n; i > 0; i--) {
+      if (i + T[i] <= n + 1) {
+        D[i] = Math.max(D[i + T[i]] + P[i], D[i + 1]);
+      } else {
+        D[i] = D[i + 1];
       }
     }
+
     System.out.println(D[1]);
   }
 }
