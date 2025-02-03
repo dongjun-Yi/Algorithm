42ac3ae4f02e3d3ce78203a900e648733f7ab8d0
dongjun-Yi <90665186+dongjun-Yi@users.noreply.github.com>
2025-02-03 22:53:57 +0900

    [Bronze I] Title: 평균, Time: 224 ms, Memory: 19884 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -1,23 +1,24 @@
 import java.util.*;
 
-public class Main {
-  public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
-    int n = scanner.nextInt();
-    int scores[] = new int[n];
-
-    for (int i = 0; i < n; i++) {
-      scores[i] = scanner.nextInt();
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] numbers = new int[n];
+        
+        for (int i =0; i < n; i++){
+            numbers[i] = sc.nextInt();
+        }
+        
+        double result = 0;
+        
+        Arrays.sort(numbers);
+        int m = numbers[n -1];
+        
+        for (int i =0; i < n; i++){
+            result += (double) numbers[i] / m * 100;
+        }
+        
+        System.out.println(result / n);
     }
-    scanner.close();
-
-    Arrays.sort(scores);
-    int max_value = scores[n - 1];
-    double result = 0;
-
-    for (int i = 0; i < n; i++) {
-      result += (double) scores[i] / max_value * 100;
-    }
-    System.out.println(result / n);
-  }
 }
